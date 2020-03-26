771db15b5c582df6b4819ef2bade42ddded2a661
rotems <rotems@weizmann.ac.il>
2020-03-26 02:14:55 +0200

    fix

---
+++ fuse_19class_cc_ce_train.sh
@@ -0,0 +1,1 @@
+/opt/conda/bin/python /home/labs/waic/rotems/code/pytorch_segmentation/train_fuse.py --config /home/labs/waic/rotems/code/pytorch_segmentation/cc_ce_config.json
---
+++ wire_fuse_ce_train.sh
@@ -0,0 +1,1 @@
+/opt/conda/bin/python /home/labs/waic/rotems/code/pytorch_segmentation/train_fuse.py --config /home/labs/waic/rotems/code/pytorch_segmentation/wire_fuse_ce_config.json 
